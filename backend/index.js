5407471844ef837a686c20270f9d332d4e135856
johnnyneverwalked <john.stamtsidis@gmail.com>
2020-09-09 16:53:48 +0300

    added landmark routes

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,44 @@
 // Example express application adding the parse-server module to expose Parse
 // compatible API routes.
 
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var ParseDashboard = require('parse-dashboard');
-var path = require('path');
+const express = require('express');
+const jsonParser = require("body-parser").json()
+const ParseServer = require('parse-server').ParseServer;
+const ParseDashboard = require('parse-dashboard');
+const path = require('path');
 require("dotenv").config({path: path.join(__dirname + "/../.env")});
 
-var databaseUri = process.env.MONGODB_URI;
+const landmarkRoutes = require("./routes/landmarkRoutes")
+
+const databaseUri = process.env.MONGODB_URI;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-var api = new ParseServer({
+const api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  serverURL: process.env.SERVER_URL || 'http://localhost:5000/parse',  // Don't forget to change to https if needed
 });
 
-var dashboard = new ParseDashboard({
+const dashboard = new ParseDashboard({
   "apps": [
     {
-      "serverURL": process.env.SERVER_URL || 'http://localhost:1337/parse',
+      "serverURL": process.env.SERVER_URL || 'http://localhost:5000/parse',
       "appId": process.env.APP_ID || 'myAppId',
       "masterKey": process.env.MASTER_KEY || '',
-      "appName": process.env.APP_NAME || "MyApp"
+      "appName": process.env.APP_NAME || "MyApp",
+      "primaryBackgroundColor": "#ac1aa5",
+      "secondaryBackgroundColor": "#1b65d4"
     }
   ]
 });
 
-var app = express();
-
+const app = express();
+app.use(jsonParser);
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
@@ -46,19 +48,17 @@ app.use('/dashboard', dashboard);
 // Serve the Parse API on the /parse URL prefix
 app.use('/parse', api);
 
+// make Landmark CRUD routes available at /landmarks
+app.use("/landmarks", landmarkRoutes)
+
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
 
-var port = process.env.SERVER_PORT || 1337;
-var httpServer = require('http').createServer(app);
+const port = process.env.SERVER_PORT || 5000;
+const httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
---
+++ backend/routes/landmarkRoutes.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = require("express").Router();
+const Parse = require("parse/node");
+Parse._initialize(process.env.APP_ID, "", process.env.MASTER_KEY)
+
+const Landark = Parse.Object.extend("Landmark")
+
+
+// ? retrieve
+router.get("/", async (req, res) => {
+  try {
+    const results = await new Parse.Query(Landark)
+      .ascending("order")
+      // .select("order")
+      .find();
+    res.json({success: true, data: results || [], total: (results || []).length})
+  } catch (e) {
+    console.error(e);
+    res.json({success: false, error: e})
+  }
+})
+
+// ? find by id
+router.get("/findById/:_id", async (req, res) => {
+  try {
+    const result = await new Parse.Query(Landark)
+      .equalTo("objectId", req.params._id)
+      .first()
+
+    res.json({success: true, data: result || null})
+  } catch (e) {
+    console.error(e);
+    res.json({success: false, error: e})
+  }
+})
+
+// ? update
+router.put("/update/:_id", async (req, res) => {
+  try {
+    // TODO validate user is admin
+
+    const result = await new Parse.Query(Landark)
+      .equalTo("objectId", req.params._id)
+      .first()
+
+    if (!result) {
+      res.json({success: false, error: `Landmark with id [${req.params._id}] not found.`})
+      return;
+    }
+
+    for (let key in (req.body || {})) {
+      // TODO prevent update from adding new columns to the schema (?????????????)
+      result.set(key, req.body[key]);
+    }
+
+    const updated = await result.save();
+
+    res.json({success: true, data: updated || null});
+  } catch (e) {
+    console.error(e);
+    res.json({success: false, error: e})
+  }
+})
+
+module.exports = router;
